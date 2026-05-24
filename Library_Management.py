404d6c25ed951fba794f42b14fc06e375f127dd4
Ayeshkant Ray <ayeshkantray@gmail.com>
2026-05-24 11:29:27 +0530

    Fix invalid choice UX, add continue to skip y/n prompt

--- Library_Management.py
+++ Library_Management.py
@@ -23,16 +23,19 @@ def insert(): # INSERTING BOOKS
     b=input("Enter Book Name: ")
     a=input("Enter Author's Name: ")
     s=1
-    cur.execute("INSERT INTO library_books values({},'{}','{}','{}',{})".format(i,b,qu,a,s))
+    query="INSERT INTO library_books values(%s,%s,%s,%s,%s)"
+    cur.execute(query, (i,b,qu,a,s))
     con.commit()
     print("Inserting Successful \n")
 
 def delete(): # DELETING BOOKS
-    b=input("Enter book name to be delete : ")
-    cur.execute("SELECT * FROM library_books WHERE Book_Name='{}'".format(b))
+    b=input("Enter book name to be delete: ")
+    query="SELECT * FROM library_books WHERE Book_Name=%s"
+    cur.execute(query, (b,))
     d=cur.fetchone()
-    print("The details of the book being deleted are-" ,d)
-    cur.execute("DELETE FROM library_books WHERE Book_Name='{}'".format(b))
+    print("The details of the book being deleted are: " ,d)
+    query="DELETE FROM library_books WHERE Book_Name=%s"
+    cur.execute(query, (b,))
     con.commit()
     print("Deleting Successful \n")
 
@@ -41,18 +44,19 @@ def search(): # SEARCHING BOOKS
                 "2. Search by Author \n"
                 "3. Search by Genre - "))
     if c==1:
-        q=input("Enter Book Name - ")
-        l="SELECT * FROM library_books WHERE Book_Name='{}'".format(q)
-        cur.execute(l)
+        q=input("Enter Book Name: ")
+        l="SELECT * FROM library_books WHERE Book_Name=%s"
+        cur.execute(l,(q,))
         d=cur.fetchone()
         if d:
             print("Search Successful \n")
         else:
             print("Not Found \n")
     elif c==2:
-        q=input("Enter Author Name - ")
+        q=input("Enter Author Name: ")
         m=0
-        cur.execute("SELECT * FROM library_books WHERE Author='{}'".format(q))
+        query="SELECT * FROM library_books WHERE Author=%s"
+        cur.execute(query, (q,))
         d=cur.fetchall()
         for i in d:
             m=1
@@ -61,9 +65,10 @@ def search(): # SEARCHING BOOKS
         if m==0:
             print("Author not Found \n")
     elif c==3:
-        q=input("Enter Genre - ")
+        q=input("Enter Genre: ")
         m=0
-        cur.execute("SELECT * FROM library_books WHERE Genre='{}'".format(q))
+        query="SELECT * FROM library_books WHERE Genre=%s"
+        cur.execute(query, (q,))
         d=cur.fetchall()
         for i in d:
             m=1
@@ -75,26 +80,28 @@ def search(): # SEARCHING BOOKS
 def borrow(): # BORROWING BOOKS
     un=int(input("Enter User ID: ")) # CHECKING WHETHER USER EXIST IN DATABASE IF NOT ENTER NEW USER
     flag=0
-    cur.execute("SELECT * FROM users WHERE UserID={}".format(un))
+    query="SELECT * FROM users WHERE UserID=%s"
+    cur.execute(query, (un,))
     d=cur.fetchone()
     if d:
         flag=1
         print("User Found \n")
     if flag==1:
-        s=("SELECT * FROM users WHERE activity<3 AND UserID={}".format(un))
-        cur.execute(s)
+        s="SELECT * FROM users WHERE activity<3 AND UserID=%s"
+        cur.execute(s, (un,))
         d=cur.fetchone()
         if d:
             b=input("Enter Book to Borrow: ")
-            l="SELECT * FROM library_books WHERE Book_Name='{}' AND STATUS=1".format(b)
-            cur.execute(l)
+            l="SELECT * FROM library_books WHERE Book_Name=%s AND STATUS=1"
+            cur.execute(l, (b,))
             f=cur.fetchone()
             if f:
-                q=("UPDATE library_books SET status=0 WHERE Book_Name='{}'".format(b))
-                cur.execute(q)
-                w=("UPDATE users SET activity=activity+1 WHERE UserID={}".format(un))
-                cur.execute(w)
-                cur.execute("INSERT INTO booking values({},'{}')".format(un,b))
+                q="UPDATE library_books SET status=0 WHERE Book_Name=%s"
+                cur.execute(q, (b,))
+                w="UPDATE users SET activity=activity+1 WHERE UserID=%s"
+                cur.execute(w, (un,))
+                query="INSERT INTO booking values(%s,%s)"
+                cur.execute(query, (un,b))
                 con.commit()
                 print("Borrowing Successful \n")
             else:
@@ -105,19 +112,21 @@ def borrow(): # BORROWING BOOKS
         print("\nNew User Registration")
         uname=input("Enter Your Name: ")
         act=0
-        cur.execute("INSERT INTO users values({},'{}',{})".format(un,uname,act))
+        query="INSERT INTO users values(%s,%s,%s)"
+        cur.execute(query, (un,uname,act))
         con.commit()
         print("Registered \n")
         b=input("Enter Book Name to Borrow: ")
-        l="SELECT * FROM library_books WHERE Book_Name = '{}' AND STATUS=1".format(b)
-        cur.execute(l)
+        l="SELECT * FROM library_books WHERE Book_Name = %s AND STATUS=1"
+        cur.execute(l, (b,))
         f=cur.fetchone()
         if f:
-            q=("UPDATE library_books SET status=0 WHERE Book_Name='{}'".format(b))
-            cur.execute(q)
-            w=("UPDATE users SET activity = activity + 1 WHERE UserID = '{}'".format(un))
-            cur.execute(w)
-            cur.execute("INSERT INTO booking values({},'{}')".format(un,b))
+            q="UPDATE library_books SET status=0 WHERE Book_Name=%s"
+            cur.execute(q, (b,))
+            w="UPDATE users SET activity = activity + 1 WHERE UserID = %s"
+            cur.execute(w, (un,))
+            query="INSERT INTO booking values(%s,%s)"
+            cur.execute(query, (un,b))
             con.commit()
             print("Borrowing Successful \n")
         
@@ -125,7 +134,8 @@ def borrow(): # BORROWING BOOKS
 def Return(): # RETURNING BOOKS
     un=int(input("Enter UsernameID: "))
     flag=0
-    cur.execute("SELECT * FROM users WHERE UserID={}".format(un))
+    query="SELECT * FROM users WHERE UserID=%s"
+    cur.execute(query, (un,))
     d=cur.fetchone()
     if d:
         flag=1
@@ -133,53 +143,57 @@ def Return(): # RETURNING BOOKS
     if flag==0:
         print("User Not Found \n")
         return
-    b=input("Enter book name which you want to Return-")
-    l="SELECT * FROM library_books WHERE Book_Name='{}'".format(b)
-    cur.execute(l)
+    b=input("Enter book name which you want to return: ")
+    l="SELECT * FROM booking WHERE UserID=%s AND BookName=%s"
+    cur.execute(l, (un,b))
     f=cur.fetchone()
     if f:
-        q=("UPDATE library_books SET status=1 WHERE Book_Name='{}'".format(b))
-        cur.execute(q)
-        w=("UPDATE users SET activity=activity-1 WHERE UserID={}".format(un))
-        cur.execute(w)
-        cur.execute("DELETE FROM booking WHERE UserID={} AND BookName='{}'".format(un,b))
+        q="UPDATE library_books SET status=1 WHERE Book_Name=%s"
+        cur.execute(q, (b,))
+        w="UPDATE users SET activity=activity-1 WHERE UserID=%s"
+        cur.execute(w, (un,))
+        query="DELETE FROM booking WHERE UserID=%s AND BookName=%s"
+        cur.execute(query, (un,b))
         con.commit()
         print("Return Successful \n")
     else:
-        print("Book Does Not Exist \n")
+        print("The Book is not issued by the User \n")
       
-def menu(char): # LIBRARY MENU
-    if char=='n':
-        print("Thank you for Visiting")
-        return
-    else:
-        print("\n1. Display books \n"
-              "2. Insert book \n"
-              "3. Delete book \n"
-              "4. Search \n"
-              "5. Borrow \n"
-              "6. Return \n"
-              "7. Exit ")
-        choice=int(input("Enter your choice: "))
-        if choice==1:
-            disbook()
-        elif choice==2:
-            insert()
-        elif choice==3:
-            delete()
-        elif choice==4:
-            search()
-        elif choice==5:
-            borrow()
-        elif choice==6:
-            Return()
-        elif choice==7:
-            print("THANK YOU FOR VISITING LIBRARY")
-            return
-        else:
-            print("Invalid choice \n")
-        ch=input("Do you wish to continue (y/n) ")
-        menu(ch)
+def menu(): # LIBRARY MENU
+    while True:
+            print("\n1. Display books \n"
+                  "2. Insert book \n"
+                  "3. Delete book \n"
+                  "4. Search \n"
+                  "5. Borrow \n"
+                  "6. Return \n"
+                  "7. Exit ")
+            
+            choice=int(input("Enter your choice: "))
+            
+            if choice==1:
+                disbook()
+            elif choice==2:
+                insert()
+            elif choice==3:
+                delete()
+            elif choice==4:
+                search()
+            elif choice==5:
+                borrow()
+            elif choice==6:
+                Return()
+            elif choice==7:
+                print("THANK YOU FOR VISITING LIBRARY")
+                break
+            else:
+                print("Invalid choice \n")
+                continue
+                
+            ch=input("Do you wish to continue (y/n) ")
+            if ch == 'n':
+                print("Thank you for visiting the Library")
+                break
     
 #MAIN PROGRAM
  
@@ -204,5 +218,4 @@ if con.is_connected():
 
 print("Welcome to Library") # LIBRARY CODE START
 display()
-ch = 'y'
-menu(ch)
+menu()
